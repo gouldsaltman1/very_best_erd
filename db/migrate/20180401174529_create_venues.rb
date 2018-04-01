eb15b7d650bbc6ff323344636435938834a2595f
Raghu Betina <raghu@firstdraft.com>
2018-04-01 17:45:29 +0000

    Generate venues resource with starter:resource

---
+++ db/migrate/20180401174529_create_venues.rb
@@ -0,0 +1,13 @@
+class CreateVenues < ActiveRecord::Migration
+  def change
+    create_table :venues do |t|
+      t.string :address
+      t.string :name
+      t.string :neighborhood
+      t.integer :bookmark_id
+
+      t.timestamps
+
+    end
+  end
+end
